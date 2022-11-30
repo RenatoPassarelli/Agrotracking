8d0c4845031b78c0ec61e0ca8e20c6cab066c7cd
CamMSwan <cameronms@al.insper.edu.br>
2022-11-30 19:41:18 -0300

    menu lateral

--- Código/js/script.js
+++ Código/js/script.js
@@ -2,21 +2,41 @@ function goBack() {
     window.history.back()
 }
 document.addEventListener('DOMContentLoaded', function() {
-menu = document.getElementsByClassName(".button")
-console.log(menu)
-botao = document.querySelector('.botao_menu')
-fechado = botao.src 
-aberto = fechado.replace('fechado','aberto')
-header = document.querySelector("header")
+    barra = document.querySelectorAll('.barra')
+    button = document.querySelectorAll(".button")
+    botao = document.querySelector('.botao_menu')
+    fechado = botao.src 
+    aberto = fechado.replace('fechado','aberto')
+    menu = document.querySelector('.barra_menu')
 
-botao.addEventListener('click', function(event) {
-    if (botao.src == fechado){
-        botao.src = aberto
-        menu.style.width= '12rem'
-    }
-    else if (botao.src == aberto){
-        botao.src = fechado
-        menu.style.width = '0rem'
-    }
-})
+    botao.addEventListener('click', function(event) {
+        if (botao.src == fechado){
+            botao.src = aberto
+            for(opcao of barra) {
+                opcao.style.width = "12rem"
+                pagina = opcao.querySelectorAll('a')
+                for(pgn of pagina){
+                    pgn.style.width = '12rem'
+                    console.log(pgn)
+                }
+            }
+            for(pgn of button){
+                pgn.style.width = '12rem'
+            }
+        }
+        else if (botao.src == aberto){
+            botao.src = fechado
+            for(opcao of barra) {
+                opcao.style.width = "0rem"
+                pagina = opcao.querySelectorAll('a')
+                for(pgn of pagina){
+                    pgn.style.width = '0rem'
+                    console.log(pgn)
+                }
+            }
+            for(pgn of button){
+                pgn.style.width = '0rem'
+            }
+        }
+    })
 })
