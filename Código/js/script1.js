8be783e1f429eb7cd271d9c07e925b56379d12ec
CamMSwan <cameronms@al.insper.edu.br>
2022-12-01 19:40:20 -0300

    Adicionar tarefas

--- Código/js/script1.js
+++ Código/js/script1.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     loc = window.location.pathname
     dir = loc.substring(0, loc.lastIndexOf('/'))
+    index = dir + '/Index.html'
     // Tipo de tarefas 
     inputs = document.querySelectorAll('input')
     opcoes = document.querySelector('.opcoes')
@@ -15,16 +16,48 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             else if(input.value.trim() != 0) {
                 input.classList.remove('error')
+                if (input == inputs[0]){
+                    tipo = input.value
+                }
+                if (input == inputs[1]){
+                    cargo = input.value
+                }
+                if (input == input[2]){
+                    data_inicio = input.value
+                }
+                if (input == input[3]){
+                    data_fim = input.value
+                }
+                if (input == input[4]){
+                    subtarefa = input.value
+                }
                 t += 1
             }
         }    
+
         if(t == inputs.length){
-            a.href = dir + '/Index.html'
+            nova_tarefa = document.createElement('li')
+            nova_tarefa.classList.add('tarefa')
+            
+            nova_tarefa_nome = document.createElement('p')
+            nova_tarefa_nome.classList.add('setor')
+            nova_tarefa_nome.style.color = '#b10f0f'
+            nova_tarefa_nome.type = 'checkbox'
+            nova_tarefa_nome.innerHTML = tipo
+            nova_tarefa.appendChild(nova_tarefa_nome)
+
+            nova_tarefa_cargo = document.createElement('h4')
+            nova_tarefa_cargo.classList.add('setor')
+            nova_tarefa_cargo.innerHTML = 'Responsável: ' + cargo
+            nova_tarefa.appendChild(nova_tarefa_cargo)
+
+            console.log(nova_tarefa)
+
+            a.href = index
         }
     })
-
     })
-    
+
 
 
 
