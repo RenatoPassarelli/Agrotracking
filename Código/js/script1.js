c81a41a0364a8f93504766422b6ac1d48ff731cf
CamMSwan <cameronms@al.insper.edu.br>
2022-12-01 18:49:20 -0300

    responsividade show

--- Código/js/script1.js
+++ Código/js/script1.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+    loc = window.location.pathname
+    dir = loc.substring(0, loc.lastIndexOf('/'))
     // Tipo de tarefas 
-    tipo = document.querySelector('.padrao')   
-    cargo = document.querySelector('.cargo')
-    sub = document.querySelector('.sub')
-    datas = document.querySelectorAll('.data')
+    inputs = document.querySelectorAll('input')
+    opcoes = document.querySelector('.opcoes')
     adicionar = document.querySelector('.confirma')
+    a = opcoes.querySelector('a')
 
     adicionar.addEventListener('click', function(){
-        if(tipo.value.trim()<=0){
-            tipo.style.borderColor = 'red'
-        }
-        if(cargo.value.trim()<=0){
-            cargo.style.borderColor = 'red'
-        }
-        if(sub.value.trim()<= 0){
-            sub.style.borderColor = 'red'
-        }
-        for (data of datas){
-            if(data.value.trim()<= 0){
-                data.style.borderColor = 'red'
+        t = 0
+        for (input of inputs){
+            if (input.value.trim() == 0){
+                input.classList.add('error') 
             }
+            else if(input.value.trim() != 0) {
+                input.classList.remove('error')
+                t += 1
+            }
+        }    
+        if(t == inputs.length){
+            a.href = dir + '/Index.html'
         }
     })
 
