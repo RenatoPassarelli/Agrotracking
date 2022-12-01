92c03bd14f131ab15c10e2037a85c62cb9a1ec65
CamMSwan <cameronms@al.insper.edu.br>
2022-12-01 16:13:40 -0300

    responsividade

--- Código/js/script1.js
+++ Código/js/script1.js
@@ -1,9 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Tipo de tarefas 
-    tipos = document.querySelector('.tipos')
-       
+    tipo = document.querySelector('.padrao')   
+    cargo = document.querySelector('.cargo')
+    sub = document.querySelector('.sub')
+    datas = document.querySelectorAll('.data')
+    adicionar = document.querySelector('.confirma')
 
-})
+    adicionar.addEventListener('click', function(){
+        if(tipo.value.trim()<=0){
+            tipo.style.borderColor = 'red'
+        }
+        if(cargo.value.trim()<=0){
+            cargo.style.borderColor = 'red'
+        }
+        if(sub.value.trim()<= 0){
+            sub.style.borderColor = 'red'
+        }
+        for (data of datas){
+            if(data.value.trim()<= 0){
+                data.style.borderColor = 'red'
+            }
+        }
+    })
+
+    })
+    
+
 
 
 
