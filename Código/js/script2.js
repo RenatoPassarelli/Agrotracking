cb4791a55acf8db0f367d90e72518d8a5ddfdae5
CamMSwan <cameronms@al.insper.edu.br>
2022-12-06 13:44:24 -0300

    frequencia

---
+++ Código/js/script2.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    loc = window.location.pathname
+    dir = loc.substring(0, loc.lastIndexOf('/'))
+    fotos = dir + '/fotos/'
+    input = document.querySelector('.barra_buscar')
+    buscar = document.querySelector('.lupa')
+    cab1 = document.querySelector('.cab1')
+    cab2 = document.querySelector('.cab2')
+    buscar.addEventListener('click', function(){
+        minuscula = input.value.toLowerCase()
+        if(minuscula == 'tarefas' ){
+            cab1.style.height = '40rem'
+            cab2.style.height = '0rem'
+        }
+        if(minuscula == 'colheita'){
+            cab2.style.height = '40rem'
+            cab1.style.height = '0rem'
+        }
+    })
+})
